86b5ac849c9a26730c0ebcf379f7ede999c17fc4
mdavlatov20 <mdavlatov393@gmail.com>
2025-01-18 09:32:47 +0500

    eslint

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ESLint xatolarini build jarayonida e'tiborsiz qoldiradi
+  },
 };
 
 export default nextConfig;
